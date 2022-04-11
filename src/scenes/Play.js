760d675ec407e2ed9dd695c66df39bde54f8d11a
Jasha Puzon <jpuzon@ucsc.edu>
2022-04-10 20:09:47 -0700

    spaceship and rocket

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -3,9 +3,16 @@ class Play extends Phaser.Scene {
         super("playScene");
     }
 
+    preload() {
+        // loading images/tile sprites
+        this.preload.image('rocket', '/assets/rocket.png');
+        this.preload.image('spaceship', '/assets/spaceship.png');
+        this.preload.image('starfield', '/assets/starfield.png');
+    }
+
     create() {
         // adding green UI background
-        this.add.rectangle(0, borderUIsize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
         // adding white borders
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
