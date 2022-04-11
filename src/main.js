ffc040325164df06bfa1b65a535d3e50d6f34f6f
Jasha Puzon <jpuzon@ucsc.edu>
2022-04-10 19:13:26 -0700

    background

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 let config = {
-    type: Phaser.CANVAS,
-    wdith: 640,
+    type: Phaser.AUTO,
+    width: 640,
     height: 480,
     scene: [ Menu, Play ]
 }
 
 let game = new Phaser.Game(config);
+// setting UI sizes
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -4,6 +4,7 @@ class Menu extends Phaser.Scene {
     }
 
     create() {
-        this.add.text(20, 20, "Rocket Patrol Play");
+        this.add.text(20, 20, "Rocket Patrol Menu");
+        this.scene.start("playScene");
     }
 }
