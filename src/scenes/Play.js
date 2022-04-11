6b8e900af5c5303bee5e6ab29d1a6cd84b8632c1
Jasha Puzon <jpuzon@ucsc.edu>
2022-04-10 22:32:42 -0700

    adding movement

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,12 +5,14 @@ class Play extends Phaser.Scene {
 
     preload() {
         // loading images/tile sprites
-        this.preload.image('rocket', '/assets/rocket.png');
-        this.preload.image('spaceship', '/assets/spaceship.png');
-        this.preload.image('starfield', '/assets/starfield.png');
+        this.load.image('rocket', '/assets/rocket.png');
+        this.load.image('spaceship', '/assets/spaceship.png');
+        this.load.image('starfield', '/assets/starfield.png');
     }
 
     create() {
+        // placing tile sprite
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
         // adding green UI background
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
         // adding white borders
@@ -18,5 +20,16 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+        // adding rocket (p1)
+        this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
+        // defining keys
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+    }
+
+    update() {
+        this.starfield.tilePositionX -= 4;
     }
 }
